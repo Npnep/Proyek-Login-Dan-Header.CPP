01dd4976dcb48895bf8cd40bb1faddd5bbe5708e
Nepal <122434469+Npnep@users.noreply.github.com>
2023-01-18 08:22:52 +0700

    Add files via upload

---
+++ head.h
@@ -0,0 +1,13 @@
+program(char judd[50]="")
+{
+if(strcmp(judd,"HEADER")==0)
+{
+cout<<"===================Biodata====================="<<endl;
+cout<<"Nama  : Nepal Atena Morrison"<<endl;
+cout<<"Kelas : X-PPLG"<<endl;
+cout<<"User  : Nepal"<<endl;
+cout<<"Pass  : nepalmor"<<endl;
+cout<<"=============Masukan User dan Pass=============="<<endl;
+getch();
+}
+}
